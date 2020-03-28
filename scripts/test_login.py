dec88e2b9182c4923fc78ed269c369a8f6070f88
jiandong.li <jiandong.li@qq.com>
2020-03-29 02:00:18 +0800

    xiangmunew

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -4,11 +4,11 @@ class TestLogin:
         assert 0
 
     def test_login_002(self):
-        assert 0
+        assert 1
 
     def test_login_003(self):
         assert 1
 
 
 # cd jenkins_run
-# allure generate report/ -o report/html --cleanhttps://github.com/lijiandonggithub/jenkins_opject.git
+# allure generate report/ -o report/html --clean https://github.com/lijiandonggithub/jenkins_opject.git
