6a317bc03b0d62c0c7a4acb60588623f47d42232
jiandong.li <jiandong.li@qq.com>
2020-03-28 22:54:02 +0800

    789632145

---
+++ scripts/test_login.py
@@ -0,0 +1,14 @@
+class TestLogin:
+
+    def test_login_001(self):
+        assert 0
+
+    def test_login_002(self):
+        assert 0
+
+    def test_login_003(self):
+        assert 1
+
+
+# cd jenkins_run
+# allure generate report/ -o report/html --cleanhttps://github.com/lijiandonggithub/jenkins_opject.git
